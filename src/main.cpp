56c1f98190d597678ea75f58e5f00bd8ac14fb7d
Brian Erickson <berickson@gmail.com>
2020-03-29 19:03:12 -0700

    Begin esp32 common, serial commands connected

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,32 @@
 
 #include "SSD1306Wire.h"
 #include <Servo.h>
+
+#include "esp32_common.h"
+
 #include <BluetoothSerial.h>
 
 // board at https://www.amazon.com/gp/product/B07DKD79Y9
+/*
 const int oled_address=0x3c;
 const int pin_oled_sda = 4;
 const int pin_oled_scl = 15;
 const int pin_oled_rst = 16;
+*/
 const int pin_crane_servo = 2;
 
-SSD1306Wire display(oled_address, pin_oled_sda, pin_oled_scl);
+//SSD1306Wire display(oled_address, pin_oled_sda, pin_oled_scl);
 Servo crane_servo;
 BluetoothSerial bluetooth;
 
 void setup() {
-  Serial.begin(921600);
+  esp32_common_setup();
   Serial.println("crane started");
 
   bluetooth.begin("crane");
 
   // initialize the display
+  /*
   pinMode(pin_oled_rst, OUTPUT);
   digitalWrite(pin_oled_rst, LOW);
   delay(10);
@@ -30,7 +36,8 @@ void setup() {
   display.init();
   display.setFont(ArialMT_Plain_10);
   display.flipScreenVertically();
-
+  */
+ 
   // initialize servo
   crane_servo.attach(pin_crane_servo);
 
@@ -40,11 +47,14 @@ void setup() {
 }
 
 void loop() {
+  esp32_common_loop();
+
   static float base_servo_increment = 0.02;
   static float servo_increment = base_servo_increment;
   static float servo_angle = 90;
   const int max_servo_angle = 117;
   const int min_servo_angle = 20;
+
 
   while(bluetooth.available()) {
     Serial.write(bluetooth.read());
